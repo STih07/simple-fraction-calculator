c5f5fd7d1b87d8cd7281865e8b7a25604120875a
stih07 <artur.momo@gmail.com>
2019-03-12 10:33:29 +0200

    Change sieve of Eratosthenes algorithm to Euclidian

--- simplifier.ts
+++ simplifier.ts
@@ -3,30 +3,13 @@ import { FractionalNumber } from "./fractional-number";
 export class Simplifier {
 
     public static simplify(num: FractionalNumber): FractionalNumber {
-        if(num.numerator % num.denominator === 0) {
-            num.numerator /= num.denominator;
-            num.denominator = 1;
-        } else if(num.denominator % num.numerator === 0) {
-            num.denominator /= num.numerator;
-            num.numerator = 1;
-        } else {
-            // TODO It really hurts on big numbers. Optimize it
-            let simpleNumbers: number[] = [];
-            for(let i = 2; i <= (Math.min(num.numerator, num.denominator)/2); i++) {
-                simpleNumbers.push(i);
-            }
-            // Just remove all not-simple numbers from the array
-            for(let i = 0; simpleNumbers[i]**2 <= (Math.min(num.numerator, num.denominator)); i++) {
-                simpleNumbers.filter(x => x % simpleNumbers[i] !== 0 || x === simpleNumbers[i]);
-            }
-            for(let number of simpleNumbers) {
-                while(num.denominator % number === 0 && num.numerator % number === 0) {
-                    num.denominator /= number;
-                    num.numerator /= number;
-                }
-            }
+        let [a, b] = [num.numerator, num.denominator];
+        while(a !== b) {
+            if(a > b) a -= b;
+            else b -= a;
         }
-
+        num.denominator /= a;
+        num.numerator /= a;
         return num;
     }
 }
