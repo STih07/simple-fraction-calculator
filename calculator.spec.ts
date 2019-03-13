ecab4d999289d64f11323fb8c505adfa65c7065c
stih07 <artur.momo@gmail.com>
2019-03-13 10:40:20 +0200

    Add calculator. Magic code

---
+++ calculator.spec.ts
@@ -0,0 +1,112 @@
+import { Calculator } from './calculator';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Calculate with one operator', () => {
+    it('Given "2/5". Should return "2/5"', () => {
+        const result = Calculator.execute("2/5");
+        expect(result).to.equal("2/5");
+    });
+    it('Given "1/5 + 2/5". Should return "3/5"', () => {
+        const result = Calculator.execute("1/5 + 2/5");
+        expect(result).to.equal("3/5");
+    });
+    it('Given "1/5 * 1/5". Should return "1/25"', () => {
+        const result = Calculator.execute("1/5 * 1/5");
+        expect(result).to.equal("1/25");
+    });
+    it('Given "1/5 / 1/5". Should return "1"', () => {
+        const result = Calculator.execute("1/5 / 1/5");
+        expect(result).to.equal("1");
+    });
+    it('Given "2 - 1/3". Should return "5/3"', () => {
+        const result = Calculator.execute("2 - 1/3");
+        expect(result).to.equal("5/3");
+    });
+    it('Given "3 * 1". Should return "3"', () => {
+        const result = Calculator.execute("3 * 1");
+        expect(result).to.equal("3");
+    });
+    it('Given "0 * 3". Should return "0"', () => {
+        const result = Calculator.execute("0 * 3");
+        expect(result).to.equal("0");
+    });
+    it('Given "2/5 / 1/5". Should return "2"', () => {
+        const result = Calculator.execute("2/5 / 1/5");
+        expect(result).to.equal("2");
+    });
+});
+
+describe('Calculate with more then one operators', () => {
+    it('Given "2/5 + 1/6 - 1/6". Should return "2/5"', () => {
+        const result = Calculator.execute("2/5 + 1/6 - 1/6");
+        expect(result).to.equal("2/5");
+    });
+    it('Given "1/5 + 3/5 * 1/3". Should return "2/5"', () => {
+        const result = Calculator.execute("1/5 + 3/5 * 1/3");
+        expect(result).to.equal("2/5");
+    });
+    it('Given "1/5 + 3/5 / 1/3". Should return "2"', () => {
+        const result = Calculator.execute("1/5 + 3/5 / 1/3");
+        expect(result).to.equal("2");
+    });
+    it('Given "1/5 * 1/5 - 1/25 + 4". Should return "4"', () => {
+        const result = Calculator.execute("1/5 * 1/5 - 1/25 + 4");
+        expect(result).to.equal("4");
+    });
+    it('Given "1/5 * 1/5 - 1/25 * 5". Should return "-4/25"', () => {
+        const result = Calculator.execute("1/5 * 1/5 - 1/25 * 5");
+        expect(result).to.equal("-4/25");
+    });
+    it('Given "1/5 / 1/5". Should return "1"', () => {
+        const result = Calculator.execute("1/5 / 1/5");
+        expect(result).to.equal("1");
+    });
+    it('Given "2 - 1/3". Should return "5/3"', () => {
+        const result = Calculator.execute("2 - 1/3");
+        expect(result).to.equal("5/3");
+    });
+    it('Given "3 * 1". Should return "3"', () => {
+        const result = Calculator.execute("3 * 1");
+        expect(result).to.equal("3");
+    });
+    it('Given "0 * 3". Should return "0"', () => {
+        const result = Calculator.execute("0 * 3");
+        expect(result).to.equal("0");
+    });
+    it('Given "2/5 / 1/5". Should return "2"', () => {
+        const result = Calculator.execute("2/5 / 1/5");
+        expect(result).to.equal("2");
+    });
+    it('Given "1/5 / 1/5 / -1/5". Should return "-5"', () => {
+        const result = Calculator.execute("1/5 / 1/5 / -1/5");
+        expect(result).to.equal("-5");
+    });
+});
+
+describe('Calculate with more then one operators and brackets', () => {
+    it('Given "(2/6 + 1/6) / 1/6". Should return "3"', () => {
+        const result = Calculator.execute("(2/6 + 1/6) / 1/6");
+        expect(result).to.equal("3");
+    });
+    it('Given "1/5 + 3/5 * (1/3 - 1/3)". Should return "1/5"', () => {
+        const result = Calculator.execute("1/5 + 3/5 * (1/3 - 1/3)");
+        expect(result).to.equal("1/5");
+    });
+    it('Given "-1/5 + (1 + (5/3 - 3/5))". Should return "28/15"', () => {
+        const result = Calculator.execute("-1/5 + (1 + (5/3 - 3/5))");
+        expect(result).to.equal("28/15");
+    });
+    it('Given "10/15 * (5/10 - 3 * (3/4 + 3/8))". Should return "-23/12"', () => {
+        const result = Calculator.execute("10/15 * (5/10 - 3 * (3/4 + 3/8))");
+        expect(result).to.equal("-23/12");
+    });
+    it('Given "(3/2 - 1/2) * (1/2 - 2/3) / (3/4 - 5/6)". Should return "2"', () => {
+        const result = Calculator.execute("(3/2 - 1/2) * (1/2 - 2/3) / (3/4 - 5/6)");
+        expect(result).to.equal("2");
+    });
+    it('Given "(3/2-1/2)*(1/2-2/3)/(3/4-5/6)". Should return "2"', () => {
+        const result = Calculator.execute("(3/2-1/2)*(1/2-2/3)/(3/4-5/6)");
+        expect(result).to.equal("2");
+    });
+});
--- calculator.ts
+++ calculator.ts
@@ -1,23 +1,54 @@
 import { FractionalNumber } from "./fractional-number";
+import { FractionalNumberAdapter } from "./fractional-number-adapter";
+import { ExpressionParser } from "./expression-parser";
+import {CalculatorService} from "./calculator-service";
+import {Simplifier} from "./simplifier";
+
+const bracketsRegexp = /\((?:[^(]*?\))/;
 
 
 export class Calculator {
+    /* Magic code, don't touch */
     public static execute(exp: string): string {
-        let bracketsRegexp = /\((?:[^(]*?\))/;
-        let fracRegexp = /^\s*(-?[\d]+\/-?[\d]+|-?[\d]+)/;
-        let operatorRegexp = /[+-/*]/;
         exp = `(${exp})`;
         let curBrackets = exp.match(bracketsRegexp);
         while(curBrackets && curBrackets.index !== undefined) {
             let curExpression = curBrackets[0].slice(1, -1);
-            let curExpLikeArray = [];
-            while(curExpression) {
-                if(curExpression.match(fracRegexp)) {
-                    let value = curExpression.match(fracRegexp)
-                }
+            let manual = ExpressionParser.parse(curExpression);
+            let primaryOperations: {x:string, index:number}[] = [];
+            manual.map((x, index) => {
+                if(x === "/" || x === "*") primaryOperations.push({x, index});
+            });
+            let counter = 0;
+            primaryOperations.map(el => {
+                let [a, b] = [manual[el.index-1-counter], manual[el.index+1-counter]];
+                let value: FractionalNumber;
+                if(el.x === "*") value = CalculatorService.mult(<FractionalNumber>a, <FractionalNumber>b);
+                else value = CalculatorService.divide(<FractionalNumber>a, <FractionalNumber>b);
+                manual.splice(el.index-1-counter, 3, value);
+                counter += 2;
+            });
+            let secondaryOperations: {x:string, index:number}[] = [];
+            manual.map((x, index) => {
+                if(x === "+" || x === "-") secondaryOperations.push({x, index});
+            });
+            counter = 0;
+            secondaryOperations.map(el => {
+                let [a, b] = [manual[el.index-1-counter], manual[el.index+1-counter]];
+                let value: FractionalNumber;
+                if(el.x === "+") value = CalculatorService.plus(<FractionalNumber>a, <FractionalNumber>b);
+                else value = CalculatorService.minus(<FractionalNumber>a, <FractionalNumber>b);
+                manual.splice(el.index-1-counter, 3, value);
+                counter += 2;
+            });
+            if(manual.length === 1) {
+                let value: FractionalNumber = Simplifier.simplify(manual[0] as FractionalNumber);
+                exp = exp.slice(0, curBrackets.index) + FractionalNumberAdapter.numberToString(value) + exp.slice(curBrackets.index + curBrackets[0].length);
+                curBrackets = exp.match(bracketsRegexp);
+            } else {
+                throw new Error('Unexpected input.')
             }
-            exp = exp.slice(0, curBrackets.index) + "0" + exp.slice(curBrackets.index + curBrackets[0].length);
-            curBrackets = exp.match(bracketsRegexp);
         }
+        return exp;
     }
 }
